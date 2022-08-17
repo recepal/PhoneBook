01a48d7bc0c6d92b5639860035e6efe0ce6dd3ad
Recep Alışkan <recepaliskan1@gmail.com>
2022-08-17 23:48:12 +0300

    test implemented

---
+++ PhoneBook.Test/ContactControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PhoneBook.API.Controller;
+using PhoneBook.API.Dtos;
+using PhoneBook.API.Services;
+using Xunit;
+
+namespace PhoneBook.Test
+{
+    public class ContactControllerTest
+    {
+        private readonly Mock<IContactService> _mockContactService;
+        private readonly ContactController _contactController;
+
+        public ContactControllerTest()
+        {
+            _mockContactService = new Mock<IContactService>();
+            _contactController = new ContactController(_mockContactService.Object);
+        }
+
+        [Fact]
+        public async Task GetContacts_Should_Return_Success()
+        {
+            _mockContactService.Setup(x => x.GetContacts())
+                              .ReturnsAsync(new List<ContactDto>() { });
+
+            var result = await _contactController.GetContacts();
+
+            Assert.Equal(200, ((ObjectResult)result).StatusCode);
+        }
+
+    }
+}
